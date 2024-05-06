e0d362eadfd99aed12f5a1d00a1893b8f2cd330f
wahyuwerayana <wahyuwerayana@gmail.com>
2024-05-07 02:34:53 +0700

    Mekanisme Dialogue
    - Penambahan 3 Script untuk mekanisme dialog
    - Penambahan animasi untuk dialog
    
    Tata cara penggunaan dialog:
    - Buat gameobject kosong (namanya dialog manager) untuk ditaruh script dialog manager, buat juga text untuk bagian nama karakter dan isi dialog, drag kedua game object tersebut ke komponen DialogueManager bagian Name Text dan Dialogue Text
    - Buat tombol untuk memulai dialog, tambahkan script dialog trigger di dalam komponen tombol tersebut, serta pada komponen 'button' drag komponen dialogue trigger (bukan dari tab asset, dari komponen pada tab inspector langsung) ke on click() lalu set parameter menjadi DialogueTrigger > TriggerDialogue. Jangan lupa isi juga banyak sentencenya serta char number (jumlah sentence sama dengan jumlah char number)
    - Terakhir tambahkan tombol next untuk melanjutkan dialognya, drag gameobject DialogueManager ke on click tombol next tersebut dan set parameter tombol tersebut menjadi DialogueManager > DisplayNextSentence.
    
    Untuk lebih jelasnya bisa ditanyakan atau bisa dilihat di Scene Testing

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+    public Animator animator;
+    private bool showWholeSentence = false;
+    private bool canClick = true;
+    private Queue<string> charName;
+    private Queue<string> sentences;
+    void Start(){
+        sentences = new Queue<string>();
+        charName = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue){
+        animator.SetBool("isOpen", true);
+        
+        charName.Clear();
+        
+        foreach (string name in dialogue.charNumber){
+            charName.Enqueue(name);
+        }
+        
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences){
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+        if(!canClick){
+            showWholeSentence = true;
+            canClick = true;
+            return;
+        } else if(canClick){
+            canClick = false;
+        }
+        if(sentences.Count == 0){
+            canClick = true;
+            EndDialogue();
+            return;
+        }
+        string currName = charName.Dequeue();
+        nameText.text = currName;
+        string sentence = sentences.Dequeue();
+        
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence){
+        dialogueText.text = "";
+        showWholeSentence = false;
+        foreach(char letter in sentence.ToCharArray()){
+            dialogueText.text += letter;
+            if(!showWholeSentence)
+            yield return new WaitForSeconds(.1f);
+        }
+        canClick = true;
+    }
+
+    void EndDialogue(){
+        animator.SetBool("isOpen", false);
+    }
+}
