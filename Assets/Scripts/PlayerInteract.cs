b19a734f861c0faffb695c368dd5b8c4055f18db
wahyuwerayana <wahyuwerayana@gmail.com>
2024-06-02 23:29:29 +0700

    Squashed commit of the following:
    commit 76ab8fa86097aac8a41138a3548a2a7b299bc243
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Sat May 18 00:23:20 2024 +0700
    
        Add files via upload
    
        fix c1
    
    commit ce33ee13552f4124db21e58b770d6e93fde1ec12
    Author: Wahyu Werayana <wahyuwerayana@gmail.com>
    Date:   Sat May 18 00:19:38 2024 +0700
    
        Delete Assets/Scenes/Chapter1.unity.meta
    
    commit dc3496dbe13861c363c7cdea476b070c40197b89
    Author: Wahyu Werayana <wahyuwerayana@gmail.com>
    Date:   Sat May 18 00:19:23 2024 +0700
    
        Delete Assets/Scenes/Chapter1.unity
    
    commit 054a621e6f567c87441d28706492bdfa69557474
    Merge: 671328b f57c69e
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Fri May 17 23:36:30 2024 +0700
    
        Merge branch 'Branch_Vin'
    
    commit f57c69ef0078149d58cdc3250c07bd82bdb2d958
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Fri May 17 22:48:10 2024 +0700
    
        17-05-24.2
    
        +asset
        +NPC interaction
        +groundmech
        +etc
    
    commit e009cad586d99b06a69535787f33c2d5189eca1f
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Fri May 17 11:58:08 2024 +0700
    
        17-05-24
    
    commit 9d104c39355b84dccb381ce85cd5a6d28aed02d1
    Merge: 5760bba 6857fb9
    Author: wahyuwerayana <wahyuwerayana@gmail.com>
    Date:   Tue May 14 10:56:01 2024 +0700
    
        Merge branch 'main' into Branch_Vin
    
    commit 5760bba4f64143947d5ae7919827ab77295c2f46
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Mon May 13 19:37:14 2024 +0700
    
        13.05.2024
    
        + drop item
        + drop prize
    
    commit 2b1cb56da09a3b33d57c91ee43e09de53dc64809
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Sun May 12 16:49:43 2024 +0700
    
        12.05.2024.2
    
        + Teleporter
        + RoomDoor
        + Door Mech
        + Lighting
        + Collider WALL
    
    commit 21a4e3b2612902fd96849e1d8ce4841527163baa
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Sun May 12 00:28:36 2024 +0700
    
        12.05.2024
    
        +Fix Teleport
    
    commit 8c7b3879fd28f8cf699cc762fb87bb7c2d10aefc
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Sat May 11 22:13:31 2024 +0700
    
        11.05.2024
    
        +Door
        +Teleporter
        +Car
    
    commit 39ab0d56206100da0c1dba9c6357c51c76fc86a5
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Fri May 10 22:58:30 2024 +0700
    
        10.05.2024.1
    
        +2D Lighting
    
    commit db5e19f86d64110e1dc7780371da5b8dd8a5dfee
    Author: Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
    Date:   Fri May 10 21:08:57 2024 +0700
    
        10.05.2024
    
        -Update TileMap 80%

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -1,10 +1,55 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerInteract : MonoBehaviour
+// {
+//     public NPCDrop npcDrop; 
+//     private bool canInteract = false; 
+
+//     private void Update()
+//     {
+//         if (canInteract && Input.GetKeyDown(KeyCode.T))
+//         {
+//             InteractWithNPC();
+//         }
+//     }
+
+//     private void OnTriggerEnter2D(Collider2D other)
+//     {
+//         if (other.CompareTag("NPC"))
+//         {
+//             canInteract = true; 
+//         }
+//     }
+
+//     private void OnTriggerExit2D(Collider2D other)
+//     {
+//         if (other.CompareTag("NPC"))
+//         {
+//             canInteract = false; 
+//         }
+//     }
+
+//     private void InteractWithNPC()
+//     {
+//         Iinventory inventory = FindObjectOfType<PlayerInventory>();
+//         if (inventory != null && inventory.Item > 0) 
+//         {
+//             inventory.Item -= 3; 
+//             npcDrop.ReceiveItemFromPlayer(); 
+//         }
+//     }
+
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
-    public NPCDrop npcDrop; 
+    private NPCDrop currentNpcDrop; 
     private bool canInteract = false; 
 
     private void Update()
@@ -19,6 +64,8 @@ public class PlayerInteract : MonoBehaviour
     {
         if (other.CompareTag("NPC"))
         {
+            currentNpcDrop = other.GetComponent<NPCDrop>();
+            Debug.Log("Player can interact with " + other.gameObject.name);
             canInteract = true; 
         }
     }
@@ -28,17 +75,19 @@ public class PlayerInteract : MonoBehaviour
         if (other.CompareTag("NPC"))
         {
             canInteract = false; 
+            currentNpcDrop = null;
+            Debug.Log("Player stopped interacting with " + other.gameObject.name);
         }
     }
 
     private void InteractWithNPC()
     {
         Iinventory inventory = FindObjectOfType<PlayerInventory>();
-        if (inventory != null && inventory.Item > 0) 
+        if (inventory != null && inventory.Item > 0 && currentNpcDrop != null) 
         {
+            Debug.Log("Player interacting with " + currentNpcDrop.gameObject.name);
             inventory.Item -= 3; 
-            npcDrop.ReceiveItemFromPlayer(); 
+            currentNpcDrop.ReceiveItemFromPlayer(); 
         }
     }
-
 }
