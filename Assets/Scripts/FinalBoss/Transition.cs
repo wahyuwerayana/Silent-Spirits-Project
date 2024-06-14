96db1dd75a7aca732758171783f3022f029c669c
Vincent Tanujaya <156357008+VuinZ@users.noreply.github.com>
2024-06-14 22:19:47 +0700

    update
    update

---
+++ Assets/Scripts/FinalBoss/Transition.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    public GameObject quizCanvas;
+    private bool playerInRange = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("nigger");
+        if (other.CompareTag("Player"))
+        {
+            quizCanvas.SetActive(true);
+            playerInRange = true;
+            Debug.Log("Player entered");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("nigger");
+        if (other.CompareTag("Player"))
+        {
+            
+            playerInRange = false;
+            quizCanvas.SetActive(false);
+            Debug.Log("Player left");
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Transitioning to ending scene.");
+            SceneManager.LoadScene("Ending");
+        }
+    }
+}
